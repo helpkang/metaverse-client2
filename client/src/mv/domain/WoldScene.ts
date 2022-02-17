0f335549fc3202ef6583eaf822a32b85756ecc08
helpkang <helpkang@gmail.com>
2022-02-17 14:31:22 +0900

    scene create 리팩토링

--- client/src/mv/domain/WoldScene.ts
+++ client/src/mv/domain/WoldScene.ts
@@ -1,10 +1,9 @@
 import Phaser from "phaser";
 import { DynamicLoadMoveBackground } from "./DynamicLoadMoveBackground";
 import { ManagePlayer } from "./ManagePlayer";
-import { MetaverseMapFactory } from "./MetaverseMap";
+import { MetaverseMap, MetaverseMapFactory } from "./MetaverseMap";
 
 export class WorldScene extends Phaser.Scene {
-
   managePlayer?: ManagePlayer;
 
   constructor() {
@@ -19,33 +18,26 @@ export class WorldScene extends Phaser.Scene {
     this.load.tilemapTiledJSON("map", "/assets/tilemaps/basemap.json");
   }
 
-
   public create() {
     // create the map
-    const map = this.make.tilemap({ key: "map" });
+    const map = this.createMap();
 
     // first parameter is the name of the tilemap in tiled
-    const tiles = map.addTilesetImage("grass-tiles-2-small", "tiles");
+    const tiles = this.createTiles(map);
 
     // creating the layers
-    map.createLayer("ground", tiles);
+    this.createLayer(map, tiles);
     // don't go out of the map
-    this.physics.world.bounds.width = map.widthInPixels;
-    this.physics.world.bounds.height = map.heightInPixels;
+    this.createWorldRange(map);
 
     // limit camera to map
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-    this.cameras.main.roundPixels = true; // avoid tile bleed
+    this.createCameraRange(map); // avoid tile bleed
 
-    const mvMap = MetaverseMapFactory.create({
-      wall: map.createLayer("wall", tiles)
-    });
-    this.managePlayer = new ManagePlayer(
-      this,
-      mvMap,
-      this.onMeetEnemy.bind(this)
-    );
-    new DynamicLoadMoveBackground().load(this)
+    const mvMap = this.createMvMap(map, tiles);
+
+    this.createPlayer(mvMap);
+
+    this.createDynamicMove();
   }
 
   public override update(time: number, delta: number) {
@@ -70,6 +62,50 @@ export class WorldScene extends Phaser.Scene {
 
     // start battle
   }
+
+  private createDynamicMove() {
+    new DynamicLoadMoveBackground().load(this);
+  }
+
+  private createPlayer(mvMap: MetaverseMap) {
+    this.managePlayer = new ManagePlayer(
+      this,
+      mvMap,
+      this.onMeetEnemy.bind(this)
+    );
+  }
+
+  private createMvMap(
+    map: Phaser.Tilemaps.Tilemap,
+    tiles: Phaser.Tilemaps.Tileset
+  ) {
+    return MetaverseMapFactory.create({
+      wall: map.createLayer("wall", tiles),
+    });
+  }
+
+  private createCameraRange(map: Phaser.Tilemaps.Tilemap) {
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.roundPixels = true;
+  }
+
+  private createWorldRange(map: Phaser.Tilemaps.Tilemap) {
+    this.physics.world.bounds.width = map.widthInPixels;
+    this.physics.world.bounds.height = map.heightInPixels;
+  }
+
+  private createLayer(
+    map: Phaser.Tilemaps.Tilemap,
+    tiles: Phaser.Tilemaps.Tileset
+  ) {
+    map.createLayer("ground", tiles);
+  }
+
+  private createTiles(map: Phaser.Tilemaps.Tilemap) {
+    return map.addTilesetImage("grass-tiles-2-small", "tiles");
+  }
+
+  private createMap() {
+    return this.make.tilemap({ key: "map" });
+  }
 }
-
-
